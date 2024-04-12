acebbcefe7f6e93fc2931e2f2f249e455f273670
30313089+Waoweens@users.noreply.github.com <30313089+Waoweens@users.noreply.github.com>
2024-04-12 19:18:53 +0700

    remove window size force

--- scenes/autoload/window_manager.gd
+++ scenes/autoload/window_manager.gd
@@ -4,9 +4,10 @@ var screen_size = DisplayServer.screen_get_size()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	pass
 	# Set the window size to 1280x720 if the screen size is greater than or equal to 1280x720
-	if (screen_size.x >= 1280 && screen_size.y >= 720):
-		get_window().set_size(Vector2(1280, 720))
+	#if (screen_size.x >= 1280 && screen_size.y >= 720):
+		#get_window().set_size(Vector2(1280, 720))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
