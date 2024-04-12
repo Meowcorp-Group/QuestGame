ad279c8383dbc1e4ec6b7e8066fdb608e62afc21
30313089+Waoweens@users.noreply.github.com <30313089+Waoweens@users.noreply.github.com>
2024-04-12 19:18:39 +0700

    wip

--- scenes/autoload/scene_manager.gd
+++ scenes/autoload/scene_manager.gd
@@ -4,25 +4,19 @@ var current_scene = null
 var scene_stack = []
 
 func _ready() -> void:
-	var root = get_tree().root
-	current_scene = root.get_child(root.get_child_count() - 1)
-	scene_stack.push_back(current_scene)
+	pass
 
 func push_scene(res_path: String) -> void:
-	scene_stack.push_back(current_scene)
-	call_deferred("_deferred_switch_scene", res_path)
+	# add element to scene stack
+	scene_stack.push_back(res_path)
+	# switch to said scene
+	switch_scene(res_path)
 
 # TODO: this is broken
 func pop_scene() -> void:
-	if scene_stack.size() > 1:
-		scene_stack.pop_back()
-		call_deferred("_deferred_switch_scene", scene_stack[scene_stack.size() - 1].get_path())
-	else:
-		print_debug("Scene stack is empty, cannot pop scene")
+	pass
 
-# legacy, will clear the scene stack
 func switch_scene(res_path: String) -> void:
-	scene_stack.clear()
 	call_deferred("_deferred_switch_scene", res_path)
 
 func _deferred_switch_scene(res_path: String) -> void:
