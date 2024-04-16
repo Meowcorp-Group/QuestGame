d747208ab9bf9a3c8d257c9f2ebdfbbae2addb59
Waoweens <30313089+Waoweens@users.noreply.github.com>
2024-04-16 10:25:12 +0700

    add collosion, movement, and interaction

--- scenes/characters/them.gd
+++ scenes/characters/them.gd
@@ -1,25 +1,18 @@
 extends CharacterBody2D
 
+const speed = 300.0
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@onready var actionable_finder = $Direction/ActionableFinder
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-
-func _physics_process(delta):
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+func _physics_process(_delta):
+	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = direction * speed
 
 	move_and_slide()
+
+func _unhandled_input(event):
+	if Input.is_action_pressed("interact"):
+		var actionables = actionable_finder.get_overlapping_areas()
+		if actionables.size() > 0:
+			actionables[0].action()
+			return
